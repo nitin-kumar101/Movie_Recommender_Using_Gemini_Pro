61cb8afbbba9b30a4f8b0be3dc1c7bee05117edf
nitin-kumar101 <67340966+nitin-kumar101@users.noreply.github.com>
2024-01-05 01:28:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from langchain.agents import create_csv_agent
-from langchain.llms import OpenAI
 from dotenv import load_dotenv
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
